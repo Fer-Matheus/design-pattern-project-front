677f6cda049a2f3548f5f8077bb8b979a3e36b6e
Matheus Freitas <f.matheus.fernandes.f@gmail.com>
2025-07-12 17:13:14 -0300

    feat: define shared user model

---
+++ src/shared/user.ts
@@ -0,0 +1,10 @@
+export type User = {
+  username: string;
+  password: string;
+  is_active: boolean;
+  is_superuser: boolean;
+  is_verified: boolean;
+  first_name: string;
+  last_name: string;
+  user_type: string;
+};
