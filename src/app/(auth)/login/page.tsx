fd9fc2ab2d0148412218a5c12122984a4049f4ee
Matheus Freitas <f.matheus.fernandes.f@gmail.com>
2025-07-15 13:57:33 -0300

    feat: update login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,17 +1,32 @@
 import Image from "next/image";
 
 import GirlRaising from "@/../public/girl-raising.png";
-import LoginPage from "@/components/pages/login-form";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import LoginForm from "@/components/pages/login/login-form";
+import RegisterForm from "@/components/pages/register/register-form";
 
 export default function Login() {
   return (
-    <div className="w-screen h-screen bg-gray-170 flex">
+    <div className="w-screen h-screen bg-gray-170 flex justify-center items-center">
       <div className="w-4/7 h-full flex justify-center items-center">
         <Image src={GirlRaising} width={550} height={300} alt="Girl raising" />
       </div>
-      <div className="w-3/7 h-screen items-center justify-center pt-15">
-        <h1 className="typing">Learnify</h1>
-        <LoginPage />
+      <div className="w-4/7 h-screen flex items-center justify-center">
+        <div className=" w-[40rem]">
+          <h1 className="typing">Learnify</h1>
+          <Tabs defaultValue="Login">
+          <TabsList>
+            <TabsTrigger value="Login">Login</TabsTrigger>
+            <TabsTrigger value="Register">Cadastre-se</TabsTrigger>
+          </TabsList>
+          <TabsContent value="Login">
+            <LoginForm/>
+          </TabsContent>
+          <TabsContent value="Register">
+            <RegisterForm/>
+          </TabsContent>
+        </Tabs>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/pages/register/register-form.tsx
@@ -0,0 +1,143 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { register } from "@/service/auth";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+export default function RegisterForm() {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [role, setRole] = useState("");
+
+  const registerOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    try {
+      
+      await register({
+        email: email,
+        password: password,
+        first_name: firstName,
+        last_name: lastName,
+        is_active: true,
+        is_superuser: false,
+        is_verified: false,
+        user_type: role
+      })
+
+    } catch (error) {
+      alert("Um erro ocorreu");
+      console.error("Login failed:", error);
+    }
+  };
+
+  return (
+    <form
+      className="flex w-full h-[45rem] rounded-xl forms-bg justify-center items-center p-5 pl-10 pr-10"
+      onSubmit={registerOnSubmit}
+    >
+      <Card className="w-full">
+        <CardHeader>
+          <CardTitle>Cadastro</CardTitle>
+          <CardDescription>
+            Crie sua conta para começar a usar a plataforma.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="grid gap-6">
+          <div className="grid gap-3">
+            <Label htmlFor="tabs-demo-current">Seu primeiro nome</Label>
+            <Input
+              id="tabs-demo-current"
+              type="text"
+              placeholder="Coloque seu primeiro nome"
+              value={firstName}
+              onChange={(value) => setFirstName(value.currentTarget.value)}
+            />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="tabs-demo-current">Seu último nome</Label>
+            <Input
+              id="tabs-demo-current"
+              type="text"
+              placeholder="Coloque seu último nome "
+              value={lastName}
+              onChange={(value) => setLastName(value.currentTarget.value)}
+            />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="tabs-demo-current">Email</Label>
+            <Input
+              id="tabs-demo-current"
+              type="text"
+              placeholder="Coloque seu email aqui"
+              value={email}
+              onChange={(value) => setEmail(value.currentTarget.value)}
+            />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="tabs-demo-new">Senha</Label>
+            <Input
+              id="tabs-demo-new"
+              type="password"
+              placeholder="Coloque sua senha aqui"
+              value={password}
+              onChange={(value) => setPassword(value.currentTarget.value)}
+            />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="tabs-demo-new">Confirmar Senha</Label>
+            <Input
+              id="tabs-demo-new"
+              type="password"
+              placeholder="Confirme sua senha aqui"
+              value={confirmPassword}
+              onChange={(value) =>
+                setConfirmPassword(value.currentTarget.value)
+              }
+            />
+          </div>
+          <div className="grid gap-3">
+            <Label htmlFor="tabs-demo-new">Tipo de usuário</Label>
+            <Select value={role} onValueChange={setRole}>
+              <SelectTrigger>
+                <SelectValue placeholder="Selecione seu estado" />
+              </SelectTrigger>
+              <SelectContent className="max-h-72 max-w-10 text-loginLabel">
+                <SelectGroup>
+                  <SelectItem key="Aluno" value="S">
+                    Aluno
+                  </SelectItem>
+                  <SelectItem key="Professor" value="T">
+                    Professor
+                  </SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+        </CardContent>
+        <CardFooter>
+          <Button type="submit">Cadastre-se</Button>
+        </CardFooter>
+      </Card>
+    </form>
+  );
+}
